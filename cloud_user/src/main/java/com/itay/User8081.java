c77e91d13937fae83e0107414c978ec1890a3223
liangyue <1115060927@qq.com>
2025-05-06 09:43:46 +0800

    1.初始化本地仓库

---
+++ cloud_user/src/main/java/com/itay/User8081.java
@@ -0,0 +1,13 @@
+package com.itay;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class User8081 {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(User8081.class,args);
+    }
+}
