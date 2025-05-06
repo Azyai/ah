1eca0c7ed8f300ab8afb8de101d5bc4abd22c113
liangyue <1115060927@qq.com>
2025-05-06 11:21:33 +0800

    1.3.2 编写全局异常处理类

---
+++ cloud-api-commons/src/main/java/com/itay/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.itay.exception;
+
+
+import com.itay.resp.ResultData;
+import com.itay.resp.ReturnCodeEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+     @ExceptionHandler(RuntimeException.class)
+     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+     public ResultData<String> exception(Exception e){
+         System.out.println("#####come in GlobalExceptionHandler");
+         return ResultData.fail(ReturnCodeEnum.RC500.getCode(),e.getMessage());
+     }
+}
