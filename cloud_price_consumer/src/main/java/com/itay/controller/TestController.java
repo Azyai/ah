73275ba147d5946a7c55695d936697b1931ae34f
liangyue <1115060927@qq.com>
2025-05-10 12:09:40 +0800

    3.2.3 cloud_price_consumer

---
+++ cloud_price_consumer/src/main/java/com/itay/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.itay.controller;
+
+import com.itay.apis.PriceApi;
+import com.itay.resp.ResultData;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Resource
+    private PriceApi priceApi;
+    @GetMapping("consumer/getServerPort")
+    public ResultData<String> getServerPort() {
+        return priceApi.getServerPort();
+    }
+
+}
