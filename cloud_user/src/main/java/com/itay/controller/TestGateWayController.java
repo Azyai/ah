6098ff6777e525cc984bf5e1d3dd93a034c2af90
liangyue <1115060927@qq.com>
2025-05-10 19:41:11 +0800

    4.3.2获取用户信息 获取用户信息的接口用于下次登录更新用户信息，如果其他端修改了，进来后自动为最新的             并且还能测试token是否过期

--- cloud_user/src/main/java/com/itay/controller/TestGateWayController.java
+++ cloud_user/src/main/java/com/itay/controller/TestGateWayController.java
@@ -1,6 +1,11 @@
 package com.itay.controller;
 
+import com.itay.entity.User;
 import com.itay.resp.ResultData;
+import com.itay.resp.UserInfo;
+import com.itay.service.UserProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class TestGateWayController {
 
+    @PreAuthorize("hasAuthority('2099')") // 只有拥有 '2099' 权限的用户才能访问
     @GetMapping("/test")
     public ResultData<String> test() {
         return ResultData.success("test");
+    }
+
+
+    @GetMapping("/test2")
+    public ResultData<String> test2() {
+        return ResultData.success("test2");
+    }
+
+
+    @Autowired
+    UserProfileService userProfileService;
+
+    @GetMapping("/getUserInfo")
+    public ResultData<UserInfo> getUserInfo(int id) {
+        UserInfo userInfo =  userProfileService.findUserProfileByUserByUserId(id);
+        return ResultData.success(userInfo);
     }
 
 }
