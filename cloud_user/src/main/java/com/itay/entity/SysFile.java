7c582000a09abf9bdc29f056c864a6c0ec518b13
liangyue <1115060927@qq.com>
2025-05-11 09:45:42 +0800

    4.4.1 文件上传记录表

---
+++ cloud_user/src/main/java/com/itay/entity/SysFile.java
@@ -0,0 +1,17 @@
+package com.itay.entity;
+
+import lombok.Data;
+import java.util.Date;
+
+@Data
+public class SysFile {
+    private Long id;
+    private String fileName;
+    private String storageName;
+    private String fileExt;
+    private String filePath;
+    private String fileType;
+    private Long fileSize;
+    private String uploadUser;
+    private Date createTime;
+}
