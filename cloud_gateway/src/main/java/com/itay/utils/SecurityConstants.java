33476cceb12fbda7b3e29749863172ac6b8c6d6b
liangyue <1115060927@qq.com>
2025-05-12 17:42:03 +0800

    5.2.3 编写Jwt工具类，和jwt参数

---
+++ cloud_gateway/src/main/java/com/itay/utils/SecurityConstants.java
@@ -0,0 +1,24 @@
+package com.itay.utils;
+
+public class SecurityConstants {
+    public static final String[] WHITE_LIST = {
+            "/api/auth/login",
+            "/api/auth/valid-register-email",
+            "/api/auth/valid-reset-email",
+            "/api/auth/start-reset",
+            "/api/auth/do-reset",
+            "/api/auth/valid-email",
+            "/api/auth/fx",
+            "/api/auth/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/swagger-resources/**",
+            "/v2/**",
+            "/v3/**",
+            "/webjars/**",
+            "/error",
+            "/static/**",
+            // 放行图标
+            "/favicon.ico",
+    };
+}
