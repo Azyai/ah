98e1511839ac957bc00208ac44a9fcf9d835c79f
liangyue <1115060927@qq.com>
2025-05-15 16:13:53 +0800

    6.2.1(2) 抽奖系统：业务代码：活动表 mapper层及其xml

---
+++ cloud_price_provider/src/main/java/com/itay/mapper/ActivityMapper.java
@@ -0,0 +1,9 @@
+package com.itay.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itay.entity.Activity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ActivityMapper extends BaseMapper<Activity> {
+}
