7086a81daff92a1e586b32dd92bfca0f1254a4b1
liangyue <1115060927@qq.com>
2025-05-23 15:20:54 +0800

    6.6.2 中奖列表：mapper及xml

--- cloud_price_provider/src/main/java/com/itay/mapper/WinningRecordMapper.java
+++ cloud_price_provider/src/main/java/com/itay/mapper/WinningRecordMapper.java
@@ -3,9 +3,12 @@ package com.itay.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.itay.entity.Participation;
 import com.itay.entity.WinningRecord;
+import jakarta.validation.constraints.NotNull;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface WinningRecordMapper extends BaseMapper<WinningRecord> {
 
+    String selectUserName(@Param("userId") @NotNull Long userId);
 }
