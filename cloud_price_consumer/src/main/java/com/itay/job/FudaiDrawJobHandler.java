b0adcd4fcaf202ee52c074b9a6c1eb1dbd4077f2
liangyue <1115060927@qq.com>
2025-05-30 11:26:08 +0800

    6.8.2（1）编写定时任务handler

---
+++ cloud_price_consumer/src/main/java/com/itay/job/FudaiDrawJobHandler.java
@@ -0,0 +1,28 @@
+package com.itay.job;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class FudaiDrawJobHandler {
+
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
+
+    @XxlJob("fudaiDrawJobHandler")
+    public void fudaiDrawJobHandler() throws Exception {
+        // 从上下文中获取activityId  假设有executorParam传递
+        String param = XxlJobHelper.getJobParam();
+        JSONObject paramJson = JSONObject.parseObject(param);
+        Integer activityId = paramJson.getInteger("activityId");
+
+        // 发送抽奖消息到mq
+        rocketMQTemplate.convertAndSend("fudai-draw-topic", activityId);
+
+    }
+}
